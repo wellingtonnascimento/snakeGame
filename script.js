013faf308a34c2e4965dc576f50d9c107cf23c8a
Wellington N <wellingto_07@hotmail.com>
2020-06-04 08:00:03 -0300

    coreção no movimento da cobrinha

--- script.js
+++ script.js
@@ -28,23 +28,25 @@ function update(event){
     if(event.keyCode == 39 && direction != "left") direction = "right";
     if(event.keyCode == 40 && direction != "up") direction = "down";
 }
+console.log(direction)
 
 function iniciarJogo(){
+
+
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
-
-    criarBG();
-    criarCobrinha();
+criarBG();
+criarCobrinha();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
     if(direction == "right") snakeX += box;
     if(direction == "left") snakeX -= box;
-    if(direction == "up") snakeY += box;
-    if(direction == "down") snakeY -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
 
     snake.pop();
 
@@ -55,5 +57,7 @@ function iniciarJogo(){
 
     snake.unshift(newHead);
 }
+
+
 
 let jogo = setInterval(iniciarJogo, 100);
